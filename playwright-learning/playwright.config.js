31ff4b652ac69ab76b1019d8c8637e400619c797
anilrapole66 <anil.rapole66@gmail.com>
2026-02-21 13:43:58 +0530

    playwright config changed

--- playwright-learning/playwright.config.js
+++ playwright-learning/playwright.config.js
@@ -74,17 +74,11 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
-
   webServer: {
-  command: 'python ymgportal/manage.py migrate && python ymgportal/manage.py runserver 127.0.0.1:8000',
+  command: '"C:\\Users\\RAPOLE ANIL KUMAR\\AppData\\Local\\Programs\\Python\\Python313\\python.exe" ../ymgportal/manage.py runserver 127.0.0.1:8000',
   url: 'http://127.0.0.1:8000',
-  timeout: 120000
-}
-  
+  timeout: 120000,
+  reuseExistingServer: false
+},
 });
 
