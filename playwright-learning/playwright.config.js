879b92cbd78479818bd7be1c1886bce590c5a375
anilrapole66 <anil.rapole66@gmail.com>
2026-02-23 13:11:05 +0530

    config changes

--- playwright-learning/playwright.config.js
+++ playwright-learning/playwright.config.js
@@ -2,45 +2,83 @@
 import { defineConfig, devices } from '@playwright/test';
 
 /**
- * IMPORTANT:
- * Jenkins cannot access "python" command
- * So we use full executable path
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
  */
+// import dotenv from 'dotenv';
+// import path from 'path';
+// dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 /**
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
   testDir: './tests',
-
+  /* Run tests in files in parallel */
   fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
-
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    /* Base URL to use in actions like `await page.goto('')`. */
     baseURL: 'http://127.0.0.1:8000/',
-    headless: true,
+    headless :true,
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
+  
 
+  /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
+
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
+
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
+
+    /* Test against mobile viewports. */
+    // {
+    //   name: 'Mobile Chrome',
+    //   use: { ...devices['Pixel 5'] },
+    // },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: { ...devices['iPhone 12'] },
+    // },
+
+    /* Test against branded browsers. */
+    // {
+    //   name: 'Microsoft Edge',
+    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    // },
+    // {
+    //   name: 'Google Chrome',
+    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    // },
   ],
 
-  /**
-   * Start Django automatically before tests
-   * Uses SQLite CI DB (settings_ci.py)
-   */
-  webServer: {
-  command: `cmd /c "cd ../ymgportal && call venv\\Scripts\\activate && set DJANGO_SETTINGS_MODULE=employee_portal.settings_ci && python manage.py migrate && python manage.py runserver 127.0.0.1:8000"`,
-  url: 'http://127.0.0.1:8000',
-  timeout: 180000,
-  reuseExistingServer: false
-},
+  /* Run your local dev server before starting the tests */
+  // webServer: {
+  //   command: 'npm run start',
+  //   url: 'http://localhost:3000',
+  //   reuseExistingServer: !process.env.CI,
+  // },
+  
 });
+
